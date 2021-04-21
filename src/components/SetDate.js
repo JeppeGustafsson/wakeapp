8f169c779ce09991ae0515c494d9fcd79d815998
jeppeGustafsson <jesper.hansgustaf@gmail.com>
2021-04-21 09:52:41 +0200

    Added missing imports

--- src/components/SetDate.js
+++ src/components/SetDate.js
@@ -1,4 +1,6 @@
 import { useState, useEffect, useCallback } from 'react';
+import {  } from '@material-ui/core';
+import {  } from '@material-ui/icons';
 import Style from '../components/SetDate.module.css';
 
 const SetDate = (props) => {
--- src/components/SetSound.js
+++ src/components/SetSound.js
@@ -1,4 +1,6 @@
 import { useState, useEffect, useCallback } from 'react';
+import {  } from '@material-ui/core';
+import {  } from '@material-ui/icons';
 import Style from '../components/SetSound.module.css';
 
 const SetSound = (props) => {
