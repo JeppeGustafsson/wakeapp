55c02272312570acc50ade44419db0219b6100fd
Cenglesson <englesson@live.se>
2021-04-22 10:58:48 +0200

    changed css

--- src/components/Info.js
+++ src/components/Info.js
@@ -10,7 +10,7 @@ const Info = (props) => {
             <TopNav />
             <section>
                 <h3>Info</h3>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer lacinia, lorem in placerat imperdiet, arcu velit tincidunt magna, ac sagittis tortor lectus ullamcorper diam. Etiam lacinia, erat porttitor faucibus vulputate, ex turpis placerat diam, eu finibus quam enim a odio. Nunc ultricies posuere dolor sit amet vestibulum. Nam ex dui, egestas id lobortis et, vehicula eu tellus. Phasellus rutrum, eros id ultrices commodo, </p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer lacinia, lorem in placerat imperdiet, arcu velit tincidunt magna, ac sagittis tortor lectus ullamcorper diam. Etiam lacinia, erat porttitor faucibus vulputate, ex turpis placerat diam, eu finibus quam enim a odio. Nunc ultricies posuere dolor sit amet vestibulum. Nam ex dui, egestas id lobortis et, vehicula eu tellus. Phasellus rutrum, eros id ultrices commodo, Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer lacinia, lorem in placerat imperdiet, arcu velit tincidunt magna, ac sagittis tortor lectus ullamcorper diam. Etiam lacinia, erat porttitor faucibus vulputate, ex turpis placerat diam, eu finibus quam enim a odio. Nunc ultricies posuere dolor sit amet vestibulum. Nam ex dui, egestas id lobortis et, vehicula eu tellus. Phasellus rutrum, eros id ultrices commodo </p>
             </section >
             <img src={logo} className={Style.Logo} />
         </div>
