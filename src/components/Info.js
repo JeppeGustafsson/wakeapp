cd9b7f27faf674f1cc608a5d5711debb6907f618
Cenglesson <englesson@live.se>
2021-04-22 14:28:11 +0200

    deleted

--- src/components/Info.js
+++ src/components/Info.js
@@ -7,7 +7,6 @@ const Info = (props) => {
 
     return (
         <div className={Style.Info}>
-            <TopNav />
             <section>
                 <h3>Info</h3>
                 <p>Markus, Cassandra, Jesper och Marica i FED20 har uppmärksammat att många använder sig av sina telefoner när de ska sätta sitt alarm. Många har problem med att vakna i tid på morgonen och det finns en stor brist på appar till mobiltelfoner som har huvudfokuset “VAKNA I TID”. </p>
