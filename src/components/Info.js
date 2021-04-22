5270afdd861be282869f21e50d91b57235a6ba15
Cenglesson <englesson@live.se>
2021-04-22 10:35:06 +0200

    added in info page

--- src/components/Info.js
+++ src/components/Info.js
@@ -1,25 +1,18 @@
-import { useState, useEffect, useCallback } from 'react';
-import { } from '@material-ui/core';
-import { } from '@material-ui/icons';
-import Style from '../components/TopNav.module.css';
+import Style from '../components/Info.module.css';
 import TopNav from './TopNav';
-import { Divider, ButtonGroup, Button, Radio } from '@material-ui/core';
 import logo from '../content/images/clock.png';
 
 
 const Info = (props) => {
 
     return (
-        <div>
+        <div className={Style.Info}>
             <TopNav />
             <section>
-                <h1>Info</h1>
+                <h3>Info</h3>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer lacinia, lorem in placerat imperdiet, arcu velit tincidunt magna, ac sagittis tortor lectus ullamcorper diam. Etiam lacinia, erat porttitor faucibus vulputate, ex turpis placerat diam, eu finibus quam enim a odio. Nunc ultricies posuere dolor sit amet vestibulum. Nam ex dui, egestas id lobortis et, vehicula eu tellus. Phasellus rutrum, eros id ultrices commodo, </p>
             </section >
-            <Divider />
-
             <img src={logo} className={Style.Logo} />
-
         </div>
 
     )
