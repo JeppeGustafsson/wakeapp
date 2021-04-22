3dcce2214660d110d53dc07c1436da695fc8c847
Cenglesson <englesson@live.se>
2021-04-22 10:20:49 +0200

    added logo in info page

--- src/components/Info.js
+++ src/components/Info.js
@@ -3,15 +3,25 @@ import { } from '@material-ui/core';
 import { } from '@material-ui/icons';
 import Style from '../components/TopNav.module.css';
 import TopNav from './TopNav';
+import { Divider, ButtonGroup, Button, Radio } from '@material-ui/core';
+import logo from '../content/images/clock.png';
+
 
 const Info = (props) => {
 
     return (
-        <section>
+        <div>
             <TopNav />
-            <h1>Info</h1>
-            <p>lorem ipsum</p>
-        </section >
+            <section>
+                <h1>Info</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer lacinia, lorem in placerat imperdiet, arcu velit tincidunt magna, ac sagittis tortor lectus ullamcorper diam. Etiam lacinia, erat porttitor faucibus vulputate, ex turpis placerat diam, eu finibus quam enim a odio. Nunc ultricies posuere dolor sit amet vestibulum. Nam ex dui, egestas id lobortis et, vehicula eu tellus. Phasellus rutrum, eros id ultrices commodo, </p>
+            </section >
+            <Divider />
+
+            <img src={logo} className={Style.Logo} />
+
+        </div>
+
     )
 };
 
